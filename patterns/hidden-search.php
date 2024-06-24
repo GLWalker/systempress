3be137f632864fa852bb00e9657ef0dbfe690626
G.L.Walker <glwalker@wsfive.com>
2024-06-24 17:35:41 -0500

    Delete patterns directory

--- patterns/hidden-search.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Title: Search
- * Slug: systempress/hidden-search
- * Inserter: no
- */
-?>
-
-<!-- wp:search {"label":"<?php echo esc_attr_x( 'Search', 'search form label', 'systempress' ); ?>","showLabel":false,"buttonText":"<?php echo esc_attr_x( 'Search', 'search button text', 'systempress' ); ?>","fontSize":"medium"} /-->
